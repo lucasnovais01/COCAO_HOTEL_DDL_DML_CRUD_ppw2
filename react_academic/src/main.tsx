2b3216099b32d9eaedfa87dce9bb3c4efe9aaf6b
lucasnovais01 <novais.oliveira@aluno.ifsp.edu.br>
2025-11-05 13:54:46 -0300

    está com tela

--- react_academic/src/main.tsx
+++ react_academic/src/main.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import { BrowserRouter } from 'react-router-dom'  // IMPORTA AQUI
 
-// IMPORTS CSS
+// CSS
 import './assets/css/style.css'
 import './assets/css/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>   {/* ENVOLVE O APP */}
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 )
