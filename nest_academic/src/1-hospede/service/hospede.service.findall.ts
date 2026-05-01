1c5d5ed835b73da832bbe63ce7eedf5d76453f30
lucasnovais01 <novais.oliveira@aluno.ifsp.edu.br>
2026-05-01 14:04:10 -0300

    PAGINATION_v2

--- nest_academic/src/1-hospede/service/hospede.service.findall.ts
+++ nest_academic/src/1-hospede/service/hospede.service.findall.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { PAGINATION } from 'src/commons/enum/paginacao.enum';
-import { Page, Pageable } from 'src/commons/pagination/page.sistema';
+import { Pageable } from 'src/commons/pagination/page.response';
+import { Page } from 'src/commons/pagination/page.sistema';
 import { Repository } from 'typeorm';
 import { ConverterHospede } from '../dto/converter/hospede.converter';
 import { HospedeResponse } from '../dto/response/hospede.response';
